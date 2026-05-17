c56a67b467ed3a12cad7892db53d73812d40aa05
corvus <inzomniaccorvus@gmail.com>
2026-05-17 22:50:47 +0600

    Changed backend for auth

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import multer from "multer";
 import { PDFParse } from "pdf-parse";
 import prisma from "./prisma.js";
 import { GoogleGenAI } from "@google/genai";
+import cookieParser from "cookie-parser";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { error } from "console";
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
@@ -12,8 +16,24 @@ const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 const app = express();
 const port = 3000;
 
+const authenticate = async (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return next();
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: decoded.userId };
+    next();
+  } catch (error) {
+    next();
+  }
+};
+
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(authenticate);
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -24,16 +44,6 @@ app.post("/submit", upload.single("resume"), async (req, res) => {
     const result = await parser.getText();
     await parser.destroy();
     const resumeText = result.text;
-    const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/i;
-    const emailMatch = resumeText.match(emailRegex);
-    if (emailMatch === null) {
-      return res.status(400).json({ error: "No email found in resume" });
-    }
-    const email = emailMatch[0];
-    let user = await prisma.user.findUnique({ where: { email } });
-    if (user === null) {
-      user = await prisma.user.create({ data: { email } });
-    }
     const jobTitle = req.body.jobTitle;
     const salaryExpectation = req.body.salaryExpectation;
     const response = await ai.models.generateContent({
@@ -42,16 +52,28 @@ app.post("/submit", upload.single("resume"), async (req, res) => {
     });
     const responseText = response.text;
     const { summary, feedback } = JSON.parse(responseText);
-    const submission = await prisma.submission.create({
-      data: {
+    if (req.user) {
+      const user = await prisma.user.findUnique({
+        where: { id: req.user.userId },
+      });
+      const submission = await prisma.submission.create({
+        data: {
+          summary,
+          feedback,
+          jobTitle,
+          salaryExpectation,
+          userID: user.id,
+        },
+      });
+      res.json({ ...submission, email: user.email });
+    } else {
+      res.json({
         summary,
         feedback,
         jobTitle,
         salaryExpectation,
-        userID: user.id,
-      },
-    });
-    res.json({ ...submission, email: user.email });
+      });
+    }
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Something went wrong." });
@@ -76,19 +98,77 @@ app.get("/submission/:id", async (req, res) => {
 
 app.get("/history", async (req, res) => {
   try {
-    const email = req.query.email;
-    const user = await prisma.user.findUnique({ where: { email } });
-    if (user === null) {
-      return res.status(404).json({ error: "entry missing." });
+    if (!req.user) {
+      return res.status(401).json({ error: "Unauthorized." });
     }
     const submissions = await prisma.submission.findMany({
-      where: { userID: user.id },
+      where: { userID: req.user.userId },
     });
     res.json(submissions);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Something went wrong." });
   }
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const hash = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: { email, password: hash },
+    });
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false, // set true when https is available ig
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.json({ message: "Registered successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "Something went wrong." });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (user === null) {
+      return res.status(404).json({ error: "entry missing." });
+    }
+    const hash = user.password;
+    const match = await bcrypt.compare(password, hash);
+    if (match) {
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false, // set true when https is available ig
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+      res.json({ message: "Logged in successfully" });
+    }
+    if (!match) {
+      return res.status(401).json({ error: "Invalid credentials." });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Something went wrong." });
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.json({
+    message: "Logged out successfully.",
+  });
 });
 
 app.listen(3000, () => {
